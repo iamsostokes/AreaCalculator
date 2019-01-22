3b756465d961c180471ef99b9b00f1d5a3476b8f
Jamez Stokes <jamezstokes@MacBook-Pro-6.local>
2019-01-22 15:57:58 -0500

    finish writing the code that will find the area of a rectangle.

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Calculator {
+    private int area;
+
+    public int setArea(int widthNum, int heightNum) {
+        area = widthNum * heightNum;
+        return area;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+
+
+}
---
+++ src/com/company/CalculatorDemo.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class CalculatorDemo {
+
+    public static void main(String[] args) {
+	// write your code here
+        Calculator calculator = new Calculator();
+
+        calculator.setArea(40, 20);
+
+        System.out.println("The area of my rectangle is "+ calculator.getArea());
+
+
+    }
+}
