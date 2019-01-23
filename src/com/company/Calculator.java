52881f22c6b5eae5a7e28c1cde9d3ff605fbec1b
Jamez Stokes <jamezstokes@macbook-pro-6.attlocal.net>
2019-01-22 22:58:46 -0500

    beginning to make changes to insert user input.

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,7 +1,11 @@
 package com.company;
 
+import java.security.PrivateKey;
+import java.util.PrimitiveIterator;
+
 public class Calculator {
     private int area;
+
 
     public int setArea(int widthNum, int heightNum) {
         area = widthNum * heightNum;
--- src/com/company/CalculatorDemo.java
+++ src/com/company/CalculatorDemo.java
@@ -1,12 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class CalculatorDemo {
 
     public static void main(String[] args) {
 	// write your code here
         Calculator calculator = new Calculator();
+        Scanner scan = new Scanner(System.in);
+        int measurement1 = scan.nextInt();
+        int measurement2 = scan.nextInt();
 
-        calculator.setArea(40, 20);
+        calculator.setArea(measurement1, measurement2);
+//        calculator.setShape();
 
         System.out.println("The area of my rectangle is "+ calculator.getArea());
 
