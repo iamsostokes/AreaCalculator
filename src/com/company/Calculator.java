d72dcc630a99aa6a228bc08f4f8c2f277c668cbc
Jamez Stokes <jamezstokes@MacBook-Pro-6.local>
2019-01-23 09:11:34 -0500

    adding the finished code for calculating the area of the triangle.

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -5,7 +5,8 @@ import java.util.PrimitiveIterator;
 
 public class Calculator {
     private int area;
-
+    private String  shape;
+    private  int area2;
 
     public int setArea(int widthNum, int heightNum) {
         area = widthNum * heightNum;
@@ -15,6 +16,24 @@ public class Calculator {
     public int getArea() {
         return area;
     }
+
+    public void setArea2(int base, int height) {
+        area2 = base * height / 2;
+    }
+
+    public int getArea2() {
+        return area2;
+    }
+
+    public void setShape(String myShape) {
+        shape =myShape;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+
 
 
 
--- src/com/company/CalculatorDemo.java
+++ src/com/company/CalculatorDemo.java
@@ -9,7 +9,10 @@ public class CalculatorDemo {
         Calculator calculator = new Calculator();
         Scanner scan = new Scanner(System.in);
         boolean isNumber;
-        System.out.println("Enter two number values the will define the width and height of a rectangle. ");
+        boolean isNumber2;
+        System.out.println("Enter two number values the will define the width and height of a rectangle ");
+
+
 
      do {
          if (scan.hasNextInt()) {
@@ -26,10 +29,27 @@ public class CalculatorDemo {
 
      } while (!isNumber);
 
+        System.out.println("Now enter two numbers to find the area of a triangle ");
+
+        do {
+            if (scan.hasNextInt()) {
+                int measurement3 = scan.nextInt();
+                System.out.println("Enter your second number. ");
+                int measurement4 = scan.nextInt();
+                calculator.setArea2(measurement3, measurement4);
+                isNumber2 = true;
+            } else {
+                System.out.println("I don't understand, please enter a number");
+                isNumber2 = false;
+                scan.next();
+            }
+
+        } while (!isNumber2);
+
 //        calculator.setShape();
 
         System.out.println("The area of my rectangle is "+ calculator.getArea());
-
+        System.out.println("The area of my triangle is " + calculator.getArea2());
 
     }
 }
